ac7b156ca15a2cdebf0600980567e87a0cb9a928
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2020-04-01 21:47:06 +0900

    Update LatticePoints.m

--- LatticePoints.m
+++ LatticePoints.m
@@ -1,5 +1,6 @@
-% wrapper function of code by Dirk Nyuens
+% wrapper function of the codes by Dirk Nyuens from
 % https://people.cs.kuleuven.be/~dirk.nuyens/fast-cbc/fastrank1pt.m
+% From the page we use fastrank1pt.m, generatorp.m, generator.m, powmod.m
 
 % n: 2^n is near to the number of points
 % s_max: the dimension
@@ -9,8 +10,8 @@ function points = LatticeRule(n,s_max)
 omega = inline('2*pi^2*(x.^2-x+1/6)');
 gamma = ones(s_max, 1) / s_max;
 beta = ones(s_max, 1);
-% the nearest prime to 2^n
-% Dickさんのコードの兼ね合いで、primes(1)=3 とした
+% primes: the nearest prime to 2^n
+% Note that we define primes(1):=3
 primes = [3, 3, 7, 13, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287, 1048573, 2097143, 4194301, 8388593, 16777213, 33554393];
 p = primes(n);
 [z,e] = fastrank1pt(p, s_max, omega, gamma, beta);
