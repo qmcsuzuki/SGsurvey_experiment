edda47be970fe7048850aa92a7a946c192914096
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2020-04-02 16:13:54 +0900

    Update Errors.m

--- Errors.m
+++ Errors.m
@@ -1,11 +1,18 @@
 function err = Errors(integ,s,weights,c,type,mmax)
 
 integrand = integ(s,weights,c);
-
 err = zeros(1,mmax);
+
 switch type
     case "Sobol"
         P = sobolset(s);
+        for m = 1:mmax
+            N = pow2(m); % Number of points
+            err(m) = abs(integrand(net(P,N)));
+        end
+    case "ScrambledSobol"
+        P = sobolset(s);
+        P = scramble(P,'MatousekAffineOwen');
         for m = 1:mmax
             N = pow2(m); % Number of points
             err(m) = abs(integrand(net(P,N)));
@@ -22,5 +29,18 @@ switch type
         for m = 1:mmax
             N = pow2(m); % Number of points
             err(m) = abs(integrand(rand(N,s)));
+        end
+    case "Halton"
+        P = haltonset(s);
+        for m = 1:mmax
+            N = pow2(m); % Number of points
+            err(m) = abs(integrand(net(P,N)));
+        end
+    case "ScrambledHalton"
+        P = haltonset(s);
+        P = scramble(P,'RR2');
+        for m = 1:mmax
+            N = pow2(m); % Number of points
+            err(m) = abs(integrand(net(P,N)));
         end
 end
