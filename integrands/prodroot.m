c9a9302ef1f07705efd3b8389914cc40741ae774
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2020-05-28 11:33:38 +0900

    Update prodroot.m

--- integrands/prodroot.m
+++ integrands/prodroot.m
@@ -1,4 +1,5 @@
-% points \mapsto QMC estimation of f(x) = exp(sum(g_j x_j))
+% points \mapsto QMC estimation of f(x) = (1+1/s)^s*\prod(x_i^{1/s})-1
+
 function func = prodroot(s,~,~)
 func = @(points) mean(prod(points(:,1:s).^(1/s),2))*(1+1/s)^s - 1;
 end
