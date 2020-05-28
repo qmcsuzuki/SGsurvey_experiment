18e5dfc7f42707813f55c1899f3f2b88aef43fea
suzuki <48762209+qmcsuzuki@users.noreply.github.com>
2020-05-28 14:01:46 +0900

    Update experiment_random.m

--- experiment_random.m
+++ experiment_random.m
@@ -1,26 +1,27 @@
+%{
+RQMC(& MC)の比較をするスクリプト
+Section 6.2 [乱択化QMCの例] が確認できる
+Scrambled Sobol, shifted lattice, Monte Carlo を比較する
+%}
+
+%以下はパラメータ（手で変更する）
 R = 10; % number of repetitions
 mmax = 20;
-s = 10;
+s = 20;
 c = 1.5;
-weights = 1./(1:s).^2;
-%weights = ones(1,s);
-integrand = @Bernoulli6;
+%weights = 1./(1:s).^2;
+weights = ones(1,s);
+integrand = @expsum;
+%ここまでパラメータ（手で変更する）
 
-%integrand = Sobolev_c(s,weights,3);
-%integrand = expsum(s,weights,0);
-%integrand = Discontinuous(s,weights,1/3);
-%integrand = Bernoulli2(s,weights,0);
-%integrand = cossum(s,weights,0);
-%integrand = G_function(s,weights,0);
-%integrand = prodroot(s,weights,0);
-
+%誤差の計算
 Sob_err = RQMCErrors(integrand,s,weights,c,"ScrambledSobol",mmax,R); 
 Lat_err = RQMCErrors(integrand,s,weights,c,"Lattice+shift",mmax,R); 
 MC_err  = RQMCErrors(integrand,s,weights,c,"Random",mmax,R); 
 
 
+% グラフを作成
 III = (1:mmax);
-
 f1 = figure;
 hold on
 plot(III, log2(Sob_err))
@@ -32,28 +33,4 @@ plot(III, -3*III)
 legend('Sobol','Lattice','MC','1/N','1/N^2','1/N^3')
 xlabel('log2(number of points)')
 ylabel('log2(Error)')
-
-
-wt = "";
-if weights == 1./(1:s).^2
-    wt = "weighted";
-end
-if func2str(integrand) == "Sobolev_c"
-    if isinteger(c)
-        sob_c = num2str(c);
-    else
-        sob_c = erase(num2str(c),".");
-    end
-else
-    sob_c = "";
-end
-
-name = strcat("Random_",func2str(integrand),sob_c,"_s",num2str(s),wt);
-SaveFigPDF(f1,name)
-
-
-
-
-
-
 
